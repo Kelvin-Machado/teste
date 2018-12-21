5edfb1b11d9b68b287852f486abe1c9bb28b3f82
Kelvin Machado <kelvinmachado@live.com>
2018-12-21 12:43:51 -0200

    Acrescentado Como vc tah?

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -11,6 +11,9 @@ namespace projeto_teste
         static void Main(string[ ] args)
         {
             Console.WriteLine("Olá, mundo!");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Como você está hoje?");
         }
     }
 }
